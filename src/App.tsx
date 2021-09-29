fd3df28c3646007621abd9f178f1488d2affd3de
byionline <10815566+byionline@users.noreply.github.com>
2021-09-29 05:33:11 +0530

    Added Navigation Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Navbar } from './components';
 const App = () => {
   return (
-    <div>
-      <h1>Hello React.</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
