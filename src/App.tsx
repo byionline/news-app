9df8ec346dabac71fffe58c7de8f50cc019f77fa
byionline <10815566+byionline@users.noreply.github.com>
2021-09-29 10:28:56 +0530

    Fix Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { HomePage, Navbar, News } from './components';
+
 import './App.css';
-import { Layout } from 'antd';
-import { Route, Switch } from 'react-router-dom';
+import { Layout, Space, Typography } from 'antd';
+import { Link, Route, Switch } from 'react-router-dom';
+const { Title } = Typography;
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
         <Navbar />
       </div>
-
       <div className="main">
         <Layout>
           <div className="routes">
@@ -24,8 +25,18 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
+        <div className="footer">
+          <Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            News <br />
+            All rights reserved
+          </Title>
+          <Space>
+            <Link to="/">Home</Link>
+
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
