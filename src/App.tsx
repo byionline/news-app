db9373427f5e00cd4ffd0c77a536c1aafe98fa37
byionline <10815566+byionline@users.noreply.github.com>
2021-09-29 05:50:10 +0530

    Added Basic Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Navbar } from './components';
+import { HomePage, Navbar, News } from './components';
 import './App.css';
+import { Layout } from 'antd';
+import { Route, Switch } from 'react-router-dom';
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
+
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,3 @@
 export { default as Navbar } from './Navbar';
+export { default as News } from './News';
+export { default as HomePage } from './HomePage';
