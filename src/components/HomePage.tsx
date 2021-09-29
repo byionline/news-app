8baf83fed337208ae5de387cabbc3cbe1c8cbb7c
byionline <10815566+byionline@users.noreply.github.com>
2021-09-29 09:28:25 +0530

    Update HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import { News } from '../components';
+
+const { Title } = Typography;
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/News">Show More</Link>
+        </Title>
+      </div>
+      <News />
+    </>
+  );
 };
 export default HomePage;
