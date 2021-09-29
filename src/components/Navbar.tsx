4082656f8eb22d0d26052276f090f115f5d4a4fd
byionline <10815566+byionline@users.noreply.github.com>
2021-09-29 10:29:38 +0530

    Fix Responsive Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { Menu, Typography, Avatar } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, BulbOutlined } from '@ant-design/icons';
+import { HomeOutlined, BulbOutlined, MenuOutlined } from '@ant-design/icons';
 import logoIcon from '../images/Bing_logo.png';
+interface Size {
+  width: number | undefined;
+  height: number | undefined;
+}
+
 const Navbar = () => {
+  const [ScreenSize, setScreenSize] = useState<Size>({
+    width: undefined,
+    height: undefined,
+  });
+  const [ActiveMenu, setActiveMenu] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () =>
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [ScreenSize]);
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -11,16 +40,24 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">News</Link>
         </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!ActiveMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item key="home" icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
+      {ActiveMenu && (
+        <Menu theme="dark">
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
 
-        <Menu.Item key="news" icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+          <Menu.Item key="news" icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 };
