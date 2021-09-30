3df83469875f9fdc8adb8de4343d4904e47c7872
byionline <10815566+byionline@users.noreply.github.com>
2021-09-30 08:34:51 +0530

    Application Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,31 @@
-import React from 'react';
-import { HomePage, Navbar, News } from './components';
+import { Grid } from '@mui/material';
 
-import './App.css';
-import { Layout, Space, Typography } from 'antd';
-import { Link, Route, Switch } from 'react-router-dom';
-const { Title } = Typography;
+import { HomePage, Navbar, News } from './components';
+import LeftSideBar from './components/LeftSideBar';
+import { Route, Switch } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div className="app">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="main">
-        <Layout>
-          <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
+    <div>
+      <Navbar />
+      <Grid container>
+        <Grid item sm={2} xs={2}>
+          <LeftSideBar />
+        </Grid>
+        <Grid item sm={10} xs={10}>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
 
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
-          </div>
-        </Layout>
-        <div className="footer">
-          <Title level={5} style={{ color: 'white', textAlign: 'center' }}>
-            News <br />
-            All rights reserved
-          </Title>
-          <Space>
-            <Link to="/">Home</Link>
-
-            <Link to="/news">News</Link>
-          </Space>
-        </div>
-      </div>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </Grid>
+      </Grid>
     </div>
   );
 };
+
 export default App;
