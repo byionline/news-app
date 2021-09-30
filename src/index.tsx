c3d7b681f117eba8422a0c78c66a3382893d1732
byionline <10815566+byionline@users.noreply.github.com>
2021-09-30 08:32:12 +0530

    Added ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import 'antd/dist/antd.css';
+
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
 ReactDOM.render(
   <Router>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </Router>,
   document.getElementById('root')
